74773f8eaf38cca47a5b2e034999e0dbad2ba11d
Oleg Brozhyna <olegbrogina@gmail.com>
2023-10-08 19:31:06 +0300

    update

--- Admin/src/main/java/com/eshop/admin/AdminApplication.java
+++ Admin/src/main/java/com/eshop/admin/AdminApplication.java
@@ -2,12 +2,15 @@ package com.eshop.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.eshop.library.*", "com.eshop.admin.*"})
+@EnableJpaRepositories(value = "com.eshop.library.repository")
+@EntityScan(value = "com.eshop.library.model")
 public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
     }
-
 }
