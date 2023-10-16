3a80b5ed36c519f9f00e37ab62a2cb1ba7da0a27
Oleg Brozhyna <olegbrogina@gmail.com>
2023-10-16 20:50:15 +0300

    update

--- Admin/src/main/java/com/eshop/admin/controller/CategoryController.java
+++ Admin/src/main/java/com/eshop/admin/controller/CategoryController.java
@@ -2,7 +2,6 @@ package com.eshop.admin.controller;
 
 
 import com.eshop.library.model.Category;
-import com.eshop.library.repository.CategoryRepository;
 import com.eshop.library.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -11,12 +10,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -56,4 +54,26 @@ public class CategoryController {
         return "redirect:/categories";
     }
 
+    @RequestMapping(value = "/findById", method = {RequestMethod.PUT, RequestMethod.GET})
+    @ResponseBody
+    public Optional<Category> findById(Long id) {
+        return categoryService.findById(id);
     }
+
+    @GetMapping("/update-category")
+    public String update(Category category, RedirectAttributes redirectAttributes) {
+        try {
+            categoryService.update(category);
+            redirectAttributes.addFlashAttribute("success", "Update successfully!");
+        } catch (DataIntegrityViolationException e1) {
+            e1.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Duplicate name of category, please check again!");
+        } catch (Exception e2) {
+            e2.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Error from server or duplicate name of category, please check again!");
+        }
+        return "redirect:/categories";
+    }
+}
+
+
