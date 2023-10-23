9447fc70a2f656ae877f11b57db455d9187de7d1
Oleg Brozhyna <olegbrogina@gmail.com>
2023-10-23 12:33:55 +0300

    update

--- Customer/src/main/java/com/eshop/customer/CustomerApplication.java
+++ Customer/src/main/java/com/eshop/customer/CustomerApplication.java
@@ -2,8 +2,20 @@ package com.eshop.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// The annotation @SpringBootApplication indicates that this class is the entry point of a Spring Boot application.
+// It scans for components, configurations, and services in the specified packages: "com.eshop.library.*" and "com.eshop.customer.*".
 
-@SpringBootApplication
+// @EnableJpaRepositories is used to enable JPA repository scanning. It specifies the base package for JPA repository interfaces,
+// which is "com.eshop.library.repository". This is where Spring Data JPA will look for repository interfaces to generate repository beans.
+
+// @EntityScan is used to specify the base package for entity classes. It tells Spring Boot where to scan for JPA entity classes,
+// which is "com.eshop.library.model".
+@SpringBootApplication(scanBasePackages = {"com.eshop.library.*", "com.eshop.customer.*"})
+@EnableJpaRepositories(value = "com.eshop.library.repository")
+@EntityScan(value = "com.eshop.library.model")
+
 public class CustomerApplication {
 
     public static void main(String[] args) {
