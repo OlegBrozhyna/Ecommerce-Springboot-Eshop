c98377246d89f1cd8cb7c3b258edd8bef8636575
Oleg Brozhyna <olegbrogina@gmail.com>
2023-10-27 14:08:26 +0300

    update

--- Customer/src/main/java/com/eshop/customer/controller/HomeController.java
+++ Customer/src/main/java/com/eshop/customer/controller/HomeController.java
@@ -19,26 +19,39 @@ import java.security.Principal;
 public class HomeController {
     private final CustomerService customerService;
 
+    // This method handles requests to the home page ("/" or "/index").
+    // It sets the "title" and "page" attributes in the model.
+    // If a user is authenticated (principal not null), it also sets the "username" and "totalItems" in the session.
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String home(Model model, Principal principal, HttpSession session) {
         model.addAttribute("title", "Home");
         model.addAttribute("page", "Home");
+
         if (principal != null) {
+            // Retrieve customer information based on the authenticated user's username.
             Customer customer = customerService.findByUsername(principal.getName());
+
+            // Set the "username" attribute in the session to the user's full name.
             session.setAttribute("username", customer.getFirstName() + " " + customer.getLastName());
+
+            // If the customer has a shopping cart, set the "totalItems" attribute in the session.
             ShoppingCart shoppingCart = customer.getCart();
             if (shoppingCart != null) {
                 session.setAttribute("totalItems", shoppingCart.getTotalItems());
             }
         }
+
+        // Return the view name "home" to render the home page.
         return "home";
     }
 
+    // This method handles requests to the "contact" page and sets the "title" and "page" attributes in the model.
     @GetMapping("/contact")
     public String contact(Model model) {
         model.addAttribute("title", "Contact");
         model.addAttribute("page", "Contact");
+
+        // Return the view name "contact-us" to render the contact page.
         return "contact-us";
     }
-
 }
--- Customer/src/main/java/com/eshop/customer/controller/LoginController.java
+++ Customer/src/main/java/com/eshop/customer/controller/LoginController.java
@@ -1,13 +1,67 @@
 package com.eshop.customer.controller;
 
+import com.eshop.library.dto.CustomerDto;
+import com.eshop.library.model.Customer;
+import com.eshop.library.service.CustomerService;
+import com.nimbusds.oauth2.sdk.Request;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
-    @GetMapping("/login")
-    public String login(Model model){
-        return "login";
+    private final CustomerService customerService; // Dependency injection for CustomerService
+    private final BCryptPasswordEncoder passwordEncoder; // Dependency injection for BCryptPasswordEncoder
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("title", "Login Page"); // Set the title attribute in the model
+        model.addAttribute("page", "Home"); // Set the page attribute in the model
+        return "login"; // Return the view name "login"
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Register"); // Set the title attribute in the model
+        model.addAttribute("page", "Register"); // Set the page attribute in the model
+        model.addAttribute("customerDto", new CustomerDto()); // Create and add a new CustomerDto to the model
+        return "register"; // Return the view name "register"
+    }
+
+    @PostMapping("/do-register")
+    public String registerCustomer(@Valid @ModelAttribute("customerDto") CustomerDto customerDto,
+                                   BindingResult result,
+                                   Model model) {
+        try {
+            if (result.hasErrors()) { // Check if there are validation errors
+                model.addAttribute("customerDto", customerDto); // Add the CustomerDto to the model
+                return "register"; // Return the "register" view to show validation errors
+            }
+            String username = customerDto.getUsername();
+            Customer customer = customerService.findByUsername(username); // Find a customer by username
+            if (customer != null) {
+                model.addAttribute("customerDto", customerDto);
+                model.addAttribute("error", "Email has been registered!"); // Add an error message to the model
+                return "register"; // Return the "register" view to show the error message
+            }
+            if (customerDto.getPassword().equals(customerDto.getConfirmPassword())) {
+                customerDto.setPassword(passwordEncoder.encode(customerDto.getPassword())); // Encode the password
+                customerService.save(customerDto); // Save the customer data
+                model.addAttribute("success", "Registered successfully!"); // Add a success message to the model
+            } else {
+                model.addAttribute("error", "Password is incorrect"); // Add an error message for password mismatch
+                model.addAttribute("customerDto", customerDto);
+                return "register"; // Return the "register" view to show the error message
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("error", "Server error, please try again later!"); // Handle server error
+        }
+        return "register"; // Return the "register" view
     }
 }
