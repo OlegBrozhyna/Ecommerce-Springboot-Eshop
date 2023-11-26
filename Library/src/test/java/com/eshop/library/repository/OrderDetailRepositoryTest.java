16ab8e6b1001aad091b7f798b49b3d5bcfd4e8ea
Oleg Brozhyna <olegbrogina@gmail.com>
2023-11-26 20:33:28 +0200

    update

--- Library/src/test/java/com/eshop/library/repository/OrderDetailRepositoryTest.java
+++ Library/src/test/java/com/eshop/library/repository/OrderDetailRepositoryTest.java
@@ -20,29 +20,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 class OrderDetailRepositoryTest {
 
     @Autowired
-    private OrderDetailRepository orderDetailRepository;
+    private OrderDetailRepository orderDetailRepository; // Autowired OrderDetailRepository for testing
 
     @Autowired
-    private TestEntityManager testEntityManager;
+    private TestEntityManager testEntityManager; // Autowired TestEntityManager for managing test data
 
     @Test
     void findAllByCustomerId() {
         // Arrange
+        // Creating a new Customer entity and persisting it to the database
         Customer customer = new Customer("user12@gamail.com", "John Doe", "12345");
         testEntityManager.persistAndFlush(customer);
 
+        // Creating two OrderDetail entities associated with the customer
         OrderDetail order1 = new OrderDetail(customer);
         OrderDetail order2 = new OrderDetail(customer);
 
+        // Persisting and flushing the OrderDetail entities to the database
         testEntityManager.persistAndFlush(order1);
         testEntityManager.persistAndFlush(order2);
 
         // Act
+        // Retrieving orders associated with the customer from the repository
         List<Order> orders = orderDetailRepository.findAllByCustomerId(customer.getId());
 
         // Assert
+        // Verifying that the list of orders is not null and has the expected size
         assertThat(orders).isNotNull();
-        assertThat(orders).hasSize(1); // Assuming two orders were created for the customer
-
+        assertThat(orders).hasSize(2); // Assuming two orders were created for the customer
     }
 }
+
